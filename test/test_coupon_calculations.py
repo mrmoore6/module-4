4b7713b7350f2ec0c2a5d556fd2832fedd55a1c1
mrmoore6 <mrmoore@dmacc.edu>
2020-09-20 17:47:20 -0500

    hello

--- test/test_coupon_calculations.py
+++ test/test_coupon_calculations.py
@@ -8,9 +8,9 @@ class test(unittest.TestCase):
         self.assertAlmostEqual(9.33, coupon.calculate_price(8.99, 5, 20), 2)
         self.assertAlmostEqual(1.17, coupon.calculate_price(4.99, 10, 10), 2)
     def test_price_under_thirty(self):
-        self.assertAlmostEqual(23.99, coupon.calculate_price(28.99, 5, 10), 2)
-        self.assertAlmostEqual(18.99, coupon.calculate_price(12.99, 5, 15), 2)
-        self.assertAlmostEqual(19.99, coupon.calculate_price(15.99, 5, 20), 2)
-        self.assertAlmostEqual(27.99, coupon.calculate_price(10.99, 10, 10), 2)
+        self.assertAlmostEqual(30.836, coupon.calculate_price(28.99, 5, 10), 2)
+        self.assertAlmostEqual(13.148, coupon.calculate_price(12.99, 5, 15), 2)
+        self.assertAlmostEqual(15.269, coupon.calculate_price(15.99, 5, 20), 2)
+        self.assertAlmostEqual(6.894, coupon.calculate_price(10.99, 10, 10), 2)
 if __name__ == '__main__':
     unittest.main()
